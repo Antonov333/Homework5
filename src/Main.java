35883464157beb95b87cc329a735b7ff60fabea3
Antonov333 <antonovsergei333@yahoo.com>
2023-02-22 15:57:06 +0300

    Homework 5 initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        taskTitle();
+        task1();}
+    public static void taskTitle () {
+
+        System.out.println("Hello!");
+        System.out.println("Just started my Homework #5 with Java ))");
+        System.out.println(" ");
+    }
+    public static void task1 () {
+        System.out.println("Task 1");
+        System.out.println("Solution to be provided soon");
+        System.out.println(" ");
+    }
+}
+
